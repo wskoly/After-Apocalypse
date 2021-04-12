a3baba73580ca5c2a5907cb0342fd309e77881c0
Wahid Sadique Koly <57163453+wskoly@users.noreply.github.com>
2021-04-13 05:57:19 +0600

    Add files via upload

---
+++ Scripts/CameraMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraMovement : MonoBehaviour
+{
+	public Transform koly;
+    public Transform light;
+	public Vector3 distance;
+    public Vector3 lightDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = koly.position + distance;
+        light.transform.position = koly.position+lightDistance;
+    }
+    public void gameRestart()
+        {
+            SceneManager.LoadScene(0);
+        }
+}
